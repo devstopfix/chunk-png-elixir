160cd6895b6848a88993c5cc8be1a75f3f82202f
DEVSTOPFIX <devstopfix@gmail.com>
2022-12-15 11:32:45 +0000

    mix new

---
+++ lib/chunk_png.ex
@@ -0,0 +1,18 @@
+defmodule ChunkPNG do
+  @moduledoc """
+  Documentation for `ChunkPNG`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ChunkPNG.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/chunk_png_test.exs
@@ -0,0 +1,8 @@
+defmodule ChunkPNGTest do
+  use ExUnit.Case
+  doctest ChunkPNG
+
+  test "greets the world" do
+    assert ChunkPNG.hello() == :world
+  end
+end
